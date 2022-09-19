088170e8c8ce02915ebc2cf6d48cc2bcd452035b
Arun Rao <arunrao86@gmail.com>
2022-09-19 11:20:10 +0530

    new changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 //cerner_2tothe5th_2022
 document.addEventListener('onbefoneunload', () => {
-    console.log("we are listening to document unload");
+    console.log("checking if we can do something here");
 });
